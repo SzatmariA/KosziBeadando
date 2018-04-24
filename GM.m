b8900522470c56fe75c611edd45e8e97ffbe34ea
SzatmariA <ati.szatmari95@gmail.com>
2018-04-24 10:00:19 +0200

    Givens forgatomatrix update

--- GM.m
+++ GM.m
@@ -1,17 +1,13 @@
 function G = GM(p,q,st,ct)
-    A = [12,-51,4;6,167,-68;-4,24,-41];
-    
-    d = sqrt((A(q,p)^2) + (A(p,p)^2));
-    cost = A(p,p)/d;
-    sint= A(q,p)/d;
+   
 
-    G = eye(length(A));
+    G = eye(q);
     
     %G = I + (cost-1)*(I(p,:)*I(p,:)' + I(q,:)*I(q,:)') + sint*(I(p,:)*I(q,:)' - I(q,:)*I(p,:)');
-    G(p,p) = cost;
-    G(q,p) = -sint;
-    G(p,q) = sint;
-    G(q,q) = cost;
+    G(p,p) = ct;
+    G(q,p) = -st;
+    G(p,q) = st;
+    G(q,q) = ct;
     
     
 end
