5c86e1f9bfeeee7d4ecaca91ebe844c215b304dd
SzatmariA <ati.szatmari95@gmail.com>
2018-04-24 15:24:49 +0200

    Barmely negyzetes matrixra mukodik a Givens

--- GQR.m
+++ GQR.m
@@ -1,21 +1,17 @@
-function [Q,R] =GQR(A) % 
+function [Q,R] =GQR(A) 
   n = size(A);
   Q = eye(n);
-  R = A;
   
-
   for p = 1:(n-1)
-    for q =p+1:n
-        
+    for q =p+1:n 
       GG = eye(n); %GG lol
       d = sqrt((A(q,p)^2) + (A(p,p)^2));
       cost = A(p,p)/d;
       sint= A(q,p)/d;
       G=GM(p,q,sint,cost);
-      %GG(p:q,p:q) = G
-      A = G*A;
-      A
-      Q = Q*G;
+      GG(p:q,p:q) = G(p:q,p:q);
+      A = GG*A;
+      Q = Q*GG;
     end
   end
 R=A;
